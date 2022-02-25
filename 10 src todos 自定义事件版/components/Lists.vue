e5d842d1e6dca79cb3cac9b10f88d5613d64a85e
hml <2665076656@qq.com>
2022-02-25 14:57:39 +0800

    todos

---
+++ 10 src todos 自定义事件版/components/Lists.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul class="todo-main">
+      <Item
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        :isCheck="isCheck"
+        :del="del"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  name: 'Lists',
+  props: ['todos', 'isCheck', 'del'],
+  components: { Item },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
