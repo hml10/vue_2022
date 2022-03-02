86ad6c955b3d102066a2bc05913c133e63ed606b
hml <2665076656@qq.com>
2022-03-02 17:49:32 +0800

    todos

---
+++ 27 src 嵌套路由 、多级路由/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>我是About的内容</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {};
+  },
+  // mounted() {
+  //   console.log(this);
+  // },
+};
+</script>
+
+<style scoped></style>
