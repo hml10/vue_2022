7b5740904c2ba75f4101f5727103032d1b8cb914
hml <2665076656@qq.com>
2022-03-03 16:59:40 +0800

    todos

---
+++ 30 src 声明式路由 props 三种方式/pages/Message.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(m, id) in messageList" :key="id">
+        <!-- <router-link to="/home/message/detail?id=666&title=你好啊！">消息001</router-link> -->
+        <!-- <router-link to="/home/message/detail/666/你好啊！">消息001</router-link> -->
+
+        <!-- 跳转路由并携带params参数，to的字符串写法，第一种： -->
+        <!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">
+          {{ m.title }}
+        </router-link> -->
+
+        <!-- 跳转路由并携带params参数，to的对象写法，第二种：(参数：[必须以name写法]，不能用path，params配置对象) -->
+        <router-link
+          :to="{
+            name: 'xiangqing',
+            params: {
+              id: m.id,
+              title: m.title,
+            },
+          }"
+        >
+          {{ m.title }}
+        </router-link>
+
+        <!-- <router-link
+          :to="{
+            path: '/home/message/detail',
+            query: {
+              id: m.id,
+              title: m.title,
+            },
+          }"
+        >
+          {{ m.title }}
+        </router-link> -->
+      </li>
+    </ul>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      messageList: [
+        { id: '001', title: '消息001' },
+        { id: '002', title: '消息002' },
+        { id: '003', title: '消息003' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
