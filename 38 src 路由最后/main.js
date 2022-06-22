3324b89b644a2c578cdc097c77191a38f2bac61d
hml10 <2665076656@qq.com>
2022-06-22 13:21:50 +0800

    todos

--- 38 src 路由最后/main.js
+++ 38 src 路由最后/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// 引入路由器对象
+import router from './router/index';
+
 Vue.config.productionTip = false;
 
 new Vue({
@@ -8,4 +11,7 @@ new Vue({
   // beforeCreate() {
   //   Vue.prototype.$bus = this;
   // },
+
+  // 注册路由器
+  router,
 }).$mount('#app');
---
+++ 38 src 路由最后/pages/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <!-- <a class="list-group-item active" href="./home-news.html">News</a> -->
+          <router-link
+            class="list-group-item"
+            active-class="active"
+            to="/home/news"
+          >
+            News
+          </router-link>
+        </li>
+        <li>
+          <!-- <a class="list-group-item" href="./home-message.html">Message</a> -->
+          <router-link
+            class="list-group-item"
+            active-class="active"
+            to="/home/message"
+          >
+            Message
+          </router-link>
+        </li>
+      </ul>
+
+      <!-- 缓存组件 include 写的是谁的组件名，多个的话写数组 -->
+      <keep-alive :include="['News', 'Message']">
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {};
+  },
+  // mounted() {
+  //   console.log(this);
+  // },
+};
+</script>
+
+<style scoped></style>
