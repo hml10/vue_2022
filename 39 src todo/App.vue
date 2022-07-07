3e9e108ec27fc85957c4ff342b6ff43b8a633ecf
hml10 <2665076656@qq.com>
2022-07-07 18:18:45 +0800

    todo

---
+++ 39 src todo/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader :addTodo="addTodo" />
+      <List :todos="todos" :checkTodo="checkTodo" :delTodo="delTodo" />
+      <MyFooter :todos="todos" :checkAll="checkAll" :clear="clear" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue';
+import MyFooter from './components/MyFooter.vue';
+import List from './components/List.vue';
+
+export default {
+  name: 'App',
+  components: { MyHeader, MyFooter, List },
+  data() {
+    return {
+      todos: [
+        { id: '001', title: '逆天而行', done: true },
+        { id: '002', title: '随风起舞', done: false },
+      ],
+    };
+  },
+  methods: {
+    // 添加
+    addTodo(todo) {
+      // console.log(todo);
+      this.todos.unshift(todo);
+    },
+
+    // 勾选
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) {
+          todo.done = !todo.done;
+        }
+      });
+    },
+
+    // 删除
+    delTodo(id) {
+      this.todos = this.todos.filter((todo) => {
+        return todo.id !== id;
+      });
+    },
+
+    // 全选或全不选
+    checkAll(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done;
+      });
+    },
+
+    // 清楚全部
+    clear() {
+      this.todos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>app</div>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader />
+      <List :todos="todos" />
+      <MyFooter />
+    </div>
+  </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader.vue';
+import MyFooter from './components/MyFooter.vue';
+import List from './components/List.vue';
+
 export default {
   name: 'App',
+  components: { MyHeader, MyFooter, List },
   data() {
-    return {};
+    return {
+      todos: [
+        { id: '001', title: '逆天而行', done: true },
+        { id: '002', title: '随风起舞', done: false },
+      ],
+    };
   },
 };
 </script>
